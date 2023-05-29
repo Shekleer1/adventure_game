e3a5d1cc296894e48be703ea62ab6de6eea9dc10
Shekleer1 <Shekleer@gmail.com>
2023-05-29 17:44:37 +0300

    changed functions, add 6 room's to a adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -1,2 +1,115 @@
 from adventure_game_functions import*
 
+def entrance():
+    print(f'''In the depths of the ancient cave, shadows danced upon weathered stone walls. 
+Whispering echoes told tales of forgotten realms.
+A mystical aura enveloped the air, beckoning brave souls to uncover its secrets and face the perils within.
+You see a giant passage which leads you in dark depth, ahed.''')
+    input('')
+    print(f'''You've alredy take's your way to a passage when suddenly, a small hole in a wall got in to your sigtht.
+You lean against it and saw a room full of bottles and beakers. Looks like an alchemy lab.
+Aquaring too much presure to the wall, make's it crumbling and soonly collapsed under your waight.
+Now you got to choose whith way should you go...''')
+    input('')
+    choice = int(input('1. Take my way through an old cave passage.\n2. Enter laboratory\n ==> '))
+    if choice == 1:
+        return 'passage'
+    elif choice == 2:
+        return 'lab'
+    
+def laboratory():
+    print(f'''Within the timeworn laboratory's dim-lit chambers, arcane contraptions stood dormant, their purpose shrouded in mystery.
+Cracked vials and faded parchments adorned the cluttered tables, remnants of forgotten experiments that echoed with the whispers of forgotten sorcery.''')
+    input('')
+    print(f'''Beneath a pile of an old rusty equioment you see a flacon with a bright red substance, seems a result of an old experiment.''')
+    choice = int(input("1. Take a potion\n2. Not risk it\n ==> "))
+    if choice == 1:
+        print("As the healing potion touched their lips, renewed vigor surged within.")
+        Link.increase('health', 3)
+        print(f"Your Health ==> {Link.get('health')}")
+        input('')
+        print(f'''Looking over you mention an armory.
+1.Go to the armory. 2. Return back to a passege way.''')
+        choice = int(input("==> "))
+        if choice == 1:
+            return "armory"
+        elif choice == 2:
+            return 'passage'
+    elif choice == 2:
+        print("It's better not to play with such ansient magic. Good choice.")
+        input('')
+        print(f'''Looking over you mention an armory.
+1.Go to the armory. 2. Return back to a passege way.''')
+        choice = int(input("==> "))
+        if choice == 1:
+            return "armory"
+        elif choice == 2:
+            return 'passage'
+    
+def armory():
+    print(f'''In the dusty recesses of the ancient armory, rows of rusted weapons stood sentinel.
+A lingering scent of valor and battles long past permeated the air.
+Shields adorned with heraldry bore witness to a bygone era of knights and conquest.''')
+    weapon_shield(0,1)
+    input('')
+    print(f'''From an armory there's 2 way's out. One of which lead to the next room, apear as trainee field.
+The other look's like a barracks door''')
+    choice = int(input("1. Trainee field. 2. Barracks"))
+    if choice == 1:
+        return 'Trainee field'
+    elif choice == 0:
+        return 'barracks'
+
+def rope_bridge():
+    print('''Deep within the heart of a cavernous abyss, a fragile rope bridge spanned the darkness.
+Dim torchlight flickered, revealing stalactites that dripped with anticipation.
+Each cautious step echoed, harmonizing with the distant whispers of forgotten spirits lurking in the shadows''')
+    choice = int(input('''Do you dare to trespass? 1. Go ahed 2. Turn back'''))
+    if choice == 1:
+        bridge_poison_calc(1, 0)
+        return 'trespass'
+    elif choice == 2: 
+        return 'return'
+
+def trainee_field():
+    print(f'''In the hidden recesses of the cave, a training field emerged, bathed in a mystical glow.
+Novice warriors honed their skills amidst rugged terrain, guided by seasoned masters.
+Echoes of clashing blades and resolute shouts reverberated, echoing through the hallowed chambers.
+''')
+    input('')
+    weapon_shield(1, 0)
+    choice = int(input(f'''two way's: 1. passage 2. empty room'''))
+    if choice == 1:
+        return 'passage'
+    elif choice == 2:
+        return 'empty room'
+    else:
+        print("Type a number!")
+        return
+
+def barracks():
+    print('''Within the cavern's embrace, a makeshift barracks stood as a haven for weary warriors.
+Flickering torches cast dancing shadows upon stone walls adorned with tattered banners.
+Tense whispers of camaraderie mingled with the distant rumblings of imminent conflict.
+''')
+    input('')
+    print(f'''In the heart of the chamber, a spectral knight stood, his ethereal armor gleaming with an otherworldly light.
+Haunting eyes pierced the darkness, revealing a tortured soul bound to the mortal realm, forever guarding ancient secrets with a spectral sword at the ready.''')
+    choice = int(input("Would you dare take this fight?\n1. Take the fight 2.Return ==> "))
+    if choice == 1:
+        hp = Link.get('health')
+        if fight(Link, Ghost_knight):
+            Link.set('health', hp)
+            print(f"After a fight Link fall in to a deep sleep and restore his HP")
+            weapon_shield(1, 0)
+        else:
+            print(f'''R.I.P Link''')
+    elif choice == 2:
+        return 'return'
+
+
+
+
+
+
+
--- adventure_game_functions.py
+++ adventure_game_functions.py
@@ -10,86 +10,85 @@ class Caracter:
     
     def increase(self, parameter, value):
         if parameter == 'attack':
-            new_attack = self.get_attack() + value
-            self.set_attack(new_attack)
+            new_attack = self.get('attack') + value
+            self.set('attack', new_attack)
         elif parameter == 'health':
-            new_health = self.get_health() + value
-            self.set_health(new_health)
+            new_health = self.get('health') + value
+            self.set('health', new_health)
         elif parameter == 'defence':
-            new_defence = self.get_defence() + value
-            self.set_defence(new_defence)
+            new_defence = self.get('defence') + value
+            self.set('defence', new_defence)
     
     def decrease(self, parameter, value):
         if parameter == 'attack':
-            new_attack = self.get_attack() - value
-            self.set_attack(new_attack)
+            new_attack = self.get('attack') - value
+            self.set('attack', new_attack)
         elif parameter == 'health':
-            new_health = self.get_health() - value
-            self.set_health(new_health)
+            new_health = self.get('health') - value
+            self.set('health', new_health)
         elif parameter == 'defence':
-            new_defence = self.get_defence() - value
-            self.set_defence(new_defence)
+            new_defence = self.get('defence') - value
+            self.set('defence', new_defence)
 
-    def get_name(self):
-        return self.name
+    def get(self, parameter):
+        if parameter == 'attack':
+            return self.attack
+        elif parameter == 'health':
+            return self.health
+        elif parameter == 'defence':
+            return self.defence
+        elif parameter == 'name':
+            return self.name
+        
+    def chance(self, parameter):
+        if parameter == 'hit':
+            chance_to_hit = 20 * self.attack + 7 * self.health
+            return chance_to_hit
+        elif parameter == 'parry':
+            chanse_to_parry = 9 * self.attack + 4 * self.health
+            return chanse_to_parry
+        elif parameter == 'block':
+            chance_to_block = 3 + self.defence + 2 * self.health
+            return chance_to_block
 
-    def get_attack(self):
-        return self.attack
+    def roll(self, parameter):
+        if parameter == 'hit':
+            i = random.randint(1, 100)
+            if i <= self.chance('hit'):
+                return True
+            else:
+                return False
+        elif parameter == 'parry':
+            i = random.randint(1, 100)
+            if i <= self.chance('parry'):
+                return True
+            else:
+                return False
+        elif parameter == 'block':
+            i = random.randint(1, 100)
+            if i <= self.chance('block'):
+                return True
+            else:
+                return False
 
-    def get_health(self):
-        return self.health
-    
-    def get_defence(self):
-        return self.defence
-
-    def chance_to_hit(self):
-        chance_to_hit = 20 * self.attack + 7 * self.health
-        return chance_to_hit
-
-    def chance_to_parry(self):
-        chanse_to_parry = 9 * self.attack + 4 * self.health
-        return chanse_to_parry
-    
-    def chance_to_block(self):
-        chance_to_block = 3 + self.defence + 2 * self.health
-        return chance_to_block
-
-    def hit_roll(self):
-        i = random.randint(1, 100)
-        if i <= self.chance_to_hit():
-            return True
-        else:
-            return False
-
-    def parry_roll(self):
-        i = random.randint(1, 100)
-        if i <= self.chance_to_parry():
-            return True
-        else:
-            return False
-    
-    def block_roll(self):
-        i = random.randint(1, 100)
-        if i <= self.chance_to_block():
-            return True
-        else:
-            return False
-
-    def set_attack(self, attack):
-        self.attack = attack
-    def set_health(self, health):
-        self.health = health
-    def set_defence(self, defence):
-        self.defence = defence
+    def set(self, parameter, value):
+        if parameter == 'attack':
+            self.attack = value
+        elif parameter == 'health':
+            self.health = value
+        elif parameter == 'defence':
+            self.defence = value
 
 Link = Caracter('Link',1.0 ,10.0 , 0.0)
 Ganon = Caracter('Ganon',2.5 ,15.0, 22.0)
+Ghost_knight = Caracter('Ghost Knight',1.5,8,15.0)
 
 def bridge_poison_calc(bridge, poison):
     if bridge == 1:
         i = random.randint(0, 100)
         if i > 70:
             print(f"\nUnfortunatly bridge brakes and you've fell in to a cold water.")
+            input('')
             alligator_sleep_calc(1, 0)
         elif i <= 70:
             print(f"\nBridge was pretty stable, you going forward")
@@ -104,13 +103,13 @@ def alligator_sleep_calc(alligator, sleep):
     if alligator == 1:
         i = random.randint(0, 100)
         if i <= 15:
-            Link.decrease('health', 1)
             print(f"\nALLIGATORS!!! ALL OVER THE PLACE!!!")
-            if Link.get_health() > 0:
+            Link.decrease('health', 1)
+            if Link.get('health') > 0:
                 print(f'''\nYou was so lucky to make it to a nearest shore. But you got bitten pretty badly.
 You feel hot blood flowing by your arm.
-Your Health --> {Link.get_health()}''')
-            elif Link.get_health() < 1:
+Your Health --> {Link.get('health')}''')
+            elif Link.get('health') < 1:
                 print(f"\nWell, it is, what it is. A hero was eaten by a bunch of stupid lizards. What a shame.")
         elif i > 15:
             print(f"Hopefully, you just got wet. It could've ended much worse...")
@@ -118,41 +117,37 @@ Your Health --> {Link.get_health()}''')
         i = random.randint(0, 100)
         if i <= 15:
             Link.decrease('defence', 1)
-            print(f"\nIt wasn't a heal potion, rather some kind of nasty ogre secretions.\nAfter vomiting you lied down and lost consciousness.\nYour Defence --> {Link.get_defence()}")
+            print(f"\nIt wasn't a heal potion, rather some kind of nasty ogre secretions.\nAfter vomiting you lied down and lost consciousness.\nYour Defence --> {Link.get('defence')}")
         elif i >= 83:
             Link.decrease('health', 1)
             print(f"\nYep, it was poison...")
-            if Link.get_health() > 0:
+            if Link.get('health') > 0:
                 print(f'''After a long sleep you finally open your eye's.\nIt's certainly a miracle, that you still alive.
 You've only lost cople decade's of your life.
-Your Health --> {Link.get_health()}''')
-            elif Link.get_health() < 1:
+Your Health --> {Link.get('health')}''')
+            elif Link.get('health') < 1:
                 print(f"That was a damn good poison, best of what you've tried so far.")
         else:
             print("Not bad, it even reminded you taste of a beer in a local tavern down the street. It wasn't poison. What a relief!")
         
 def weapon_shield(weapon, shield):
     if weapon == 1:
-        Link.increase('health', 1)
-        Link.increase('attack', 1) 
+        Link.increase('health', 4)
+        Link.increase('attack', 2) 
         print(f'''You've been gazing into the void of the room when suddenly something starts sparking behind the rock.
 It's an old sword, a silent witness of days gone by.
 As you pick it up, you feel its heavy weight, and the might of all its previous owners fills your arms.
 You obtain a power that you couldn't even imagine before
-Health --> {Link.get_health()}
-Attack --> {Link.get_attack()}''')
+Health --> {Link.get('health')}
+Attack --> {Link.get('attack')}''')
     if shield == 1:
-        Link.increase('health', 1)
-        Link.increase('defence', 1)
+        Link.increase('health', 4)
+        Link.increase('defence', 2)
         print(f'''Wadeing through endless labyrints of an old dungeon, you suddenly realize there's might be a danger ahed.
 Why else there're so many bone's lie's all over?...
 After quick look around, you've decided to pick up a round hoplite shield, which fortunately fortunately lay near.
-Health --> {Link.get_health()}
-Defence --> {Link.get_defence()}''')
-
-def potion(heal_potion):
-    if heal_potion == 1:
-        Link.increase('health', 1)
+Health --> {Link.get('health')}
+Defence --> {Link.get('defence')}''')
 
 def get_recognize():
     i = random.randint(1, 100)
@@ -162,46 +157,51 @@ def get_recognize():
         return False
 
 def punch(attacker, defender):
-    if attacker.hit_roll():
-        print(f"{attacker.get_name()} try's to slash meat out of a {defender.get_name()}'s bones")
-        if defender.parry_roll():
-            print(f"Right in the eye! What a parry!\n{attacker.get_name()}'s lost {attacker.get_attack() / 2} Health")
-            attacker.decrease('health', attacker.get_attack() / 2)
-            print(f"{attacker.get_name()} remain's {attacker.get_health()} HP")
-        elif defender.block_roll():
-            print(f"It was a good try, but {defender.get_name()} blocked attack")
-            print(f"{defender.get_name()} remain's {defender.get_health()} HP")
+    if attacker.roll('hit'):
+        print(f"{attacker.get('name')} try's to slash meat out of a {defender.get('name')}'s bones")
+        if defender.roll('parry'):
+            print(f"Right in the eye! What a parry!\n{attacker.get('name')}'s lost {Link.get('attack') / 2} Health")
+            attacker.decrease('health', Link.get('attack') / 2)
+            print(f"{attacker.get('name')} remain's {attacker.get('health')} HP")
+        elif defender.roll('block'):
+            print(f"It was a good try, but {defender.get('name')} blocked attack")
+            print(f"{defender.get('name')} remain's {defender.get('health')} HP")
         else:
-            print(f"This hit land's right in target, {defender.get_name()} lost {attacker.get_attack()} HP")
-            defender.decrease('health', attacker.get_attack())
-            print(f"{defender.get_name()} remain's {defender.get_health()} HP")
+            print(f"This hit land's right in target, {defender.get('name')} lost {Link.get('attack')} HP")
+            defender.decrease('health', Link.get('attack'))
+            print(f"{defender.get('name')} remain's {defender.get('health')} HP")
     else:
-        print(f"What a shame, {attacker.get_name()} can't even pull up his weapon..") 
+        print(f"What a shame, {attacker.get('name')} can't even pull up his weapon..") 
 
 def fight(aggressor, victim):
     if get_recognize():
-        while aggressor.get_health() > 0 and victim.get_health() > 0:
+        while aggressor.get('health') > 0 and victim.get('health') > 0:
             punch(victim, aggressor)
             input('')
-            if aggressor.get_health() > 0 and victim.get_health() > 0:
+            if aggressor.get('health') > 0 and victim.get('health') > 0:
                 punch(aggressor, victim)
                 input('')
-        if Link.get_health() == 0 or Link.get_health() < 0:
+        if Link.get('health') == 0 or Link.get('health') < 0:
             print(f"Link gasped for breath, blood staining his trembling hands.\nWith a final sigh, his eyes dimmed, his legend coming to an end.")
             input('')
+            return False
         else:
-            print(f"{victim.get_name()}'s menacing presence faltered as he fell to his knees, his once formidable strength fading.\nWith a pained groan, darkness claimed him, his reign of terror extinguished.")
+            print(f"{victim.get('name')}'s menacing presence faltered as he fell to his knees, his once formidable strength fading.\nWith a pained groan, darkness claimed him, his reign of terror extinguished.")
             input('')
+            return True
     else:
-        while aggressor.get_health() > 0 and victim.get_health() > 0:
+        while aggressor.get('health') > 0 and victim.get('health') > 0:
             punch(aggressor, victim)
             input('')
-            if aggressor.get_health() > 0 and victim.get_health() > 0:
+            if aggressor.get('health') > 0 and victim.get('health') > 0:
                 punch(victim, aggressor)
                 input('')
-        if Link.get_health() == 0 or Link.get_health() < 0:
+        if Link.get('health') == 0 or Link.get('health') < 0:
             print(f"Link gasped for breath, blood staining his trembling hands.\nWith a final sigh, his eyes dimmed, his legend coming to an end.")
             input('')
+            return False
         else:
-            print(f"{victim.get_name()}'s menacing presence faltered as he fell to his knees, his once formidable strength fading.\nWith a pained groan, darkness claimed him, his reign of terror extinguished.")
+            print(f"{victim.get('name')}'s menacing presence faltered as he fell to his knees, his once formidable strength fading.\nWith a pained groan, darkness claimed him, his reign of terror extinguished.")
             input('')
+            return True
+
